e49c3e0035ed95a5de2b8c22debfdaecb838c1bc
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-20 17:02:27 +0300

    update

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ from transformers import BloomTokenizerFast, BloomForCausalLM
 
 def get_Model():
     model_name = "bigscience/bloomz-560m"
-   
     tokenizer = BloomTokenizerFast.from_pretrained(model_name)
     model = BloomForCausalLM.from_pretrained(
         model_name, 
