b9f14951af80bede3fca28fcd9b69a2195a9faa5
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-20 15:04:38 +0300

    update training

--- training.py
+++ training.py
@@ -5,7 +5,7 @@ def Training_Arguments():
         output_dir="bloomspellCorrection",
         learning_rate=2e-5,
         per_device_train_batch_size=2,
-        num_train_epochs=2,
+        num_train_epochs=4,
         weight_decay=0.01,
         fp16=True,
         optim="adafactor",
